9ad3df0c716a1ac26882794e08520a4a11e93839
Tony Dias Jr <141673033+tonydiasjr@users.noreply.github.com>
2025-08-17 16:14:53 -0700

    Add files via upload

---
+++ main model1.py
@@ -0,0 +1,15 @@
+print("hello world")
+print("   /|")
+print("  / |")
+print(" /__|")
+character_name = "hank"
+character_age = "48"
+print("there once a man named" + character_name)
+print("he was" + character_age+" years old")
+print("he liked the sound of his name")
+print("but didnt like the age of" + character_age)
+phrase = "giraffe academy"
+print(phrase.index("g"))
+print(3*(4+5))
+my_num =5
+print(str(my_num) + " my favorite number")
